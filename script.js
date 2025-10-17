25598664baaa7c7464aa0098b3e4d788a458ad77
AYOBAMI-JOSHUA <ayobamij89@gmail.com>
2025-10-18 00:12:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateTime() {
+  const timeEl = document.getElementById("user-time");
+  timeEl.textContent = Date.now();
+}
+
+// Initial render
+updateTime();
+
+// Optional: update every second
+setInterval(updateTime, 1000);
